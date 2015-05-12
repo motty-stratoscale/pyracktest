3c5f21bb85a59e3b56d4242e5df5826f5e994a56
Motty Porat <motty@stratoscale.com>
2015-05-12 15:30:08 +0300

    Allow to compate Host objects

--- py/strato/racktest/hostundertest/host.py
+++ py/strato/racktest/hostundertest/host.py
@@ -13,6 +13,15 @@ class Host:
         self.ssh = connection.Connection(** rackattackNode.rootSSHCredentials())
         self.__plugins = {}
 
+    def __eq__(self, other):
+        # This method is needed in order to allow:  if host1 in [host-list]...
+        if self is other:
+            return True
+        if isinstance(other, Host) and self.name == other.name:
+            raise Exception("A new Host object was created for the same host. " \
+                "You can no longer rely on custom attributes assigned to these objects.")
+        return False
+
     def __getattr__(self, name):
         if name not in self.__plugins:
             self.__plugins[name] = plugins.plugins[name](self)
